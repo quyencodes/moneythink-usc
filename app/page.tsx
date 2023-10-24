4ecc671a3496cb10c0106f799b82fdb21149fedc
Quyen Hoang <qduhoang@gmail.com>
2023-10-23 19:30:15 -0700

    added logo and routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Header from '@components/header';
 
 export default function Home() {
   return (
     <>
-      <div className="font-titleFont">test</div>
+      <Header />
     </>
   );
 }
